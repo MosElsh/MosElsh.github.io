ca7253627851178a647c707221b2392e06f7f16f
MosElsh <moselsh1@gmail.com>
2022-11-11 16:00:44 +0000

    Removed App

--- Todo-app/todo-list/src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { TaskList } from './components/task_component';
-
-const root = ReactDOM.createRoot(document.getElementsByClassName('tasks_section')[0]);
-root.render(<TaskList />);
